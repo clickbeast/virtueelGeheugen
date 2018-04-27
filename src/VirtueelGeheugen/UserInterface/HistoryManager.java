b7f24f667e790c534bb3cd97175b619917c7c43e
simonvermeir <clickbeast@gmail.com>
2018-04-28 01:27:17 +0200

    Reading from simulation

--- src/VirtueelGeheugen/UserInterface/HistoryManager.java
+++ src/VirtueelGeheugen/UserInterface/HistoryManager.java
@@ -1,6 +1,7 @@
 package VirtueelGeheugen.UserInterface;
 
 
+import javax.sound.midi.Soundbank;
 import java.util.ArrayList;
 
 //keeps track of the previous states
@@ -25,6 +26,8 @@ public class HistoryManager  {
 
     //adds a new state
     public void addNewState(UIState state) {
+
+        System.out.println("ADDING NEW STATE TO HISTORY");
         previousStates.add(state);
         //set back to the present
         this.backToPresent();
@@ -35,8 +38,12 @@ public class HistoryManager  {
         this.currentIndex = 0;
     }
 
+
     public void backToPresent() {
+        System.out.println("Current Size:"  + this.previousStates.size());
         currentIndex = this.previousStates.size() - 1;
+
+        System.out.println("Current Index:" + this.currentIndex);
     }
 
     public UIState latestState() {
@@ -55,15 +62,21 @@ public class HistoryManager  {
         return this.previousStates.get(this.currentIndex);
     }
 
+
+
     //gives back the previous state
-    public UIState previousState() {
+    public UIState goToPreviousState() {
+
+        System.out.println("PREVIOUS STATE DEMANDED");
 
         if(this.previousStates.size() == 0) {
             return  null;
         }
 
         if(this.currentIndex - 1 >= 0) {
-            this.currentIndex = this.currentIndex -1;
+            System.out.println("Current Index:" + this.currentIndex);
+
+            this.currentIndex = this.currentIndex - 1;
         }
 
         System.out.println("GETTting previous state with index + " + this.currentIndex);
@@ -73,17 +86,36 @@ public class HistoryManager  {
     }
 
 
+    public UIState accessPreviousState() {
+        System.out.println("ACCESSING PREVIOUS STATE");
+        if(this.currentIndex - 1 >= 0) {
+            System.out.println("Current Index:" + this.currentIndex);
+            return this.previousStates.get(this.currentIndex - 1);
+        }
+
+        return null;
+    }
+
+
     public boolean isFirstState() {
+    System.out.println("CHECKING IS FIRST STATE");
+        System.out.println(currentIndex);
         return currentIndex == 0;
     }
 
     public boolean isLastState() {
-        System.out.println(currentIndex);
+        System.out.println("checking is last state->" + currentIndex);
         System.out.println(this.previousStates.size() - 1);
+
+        if(currentIndex == this.previousStates.size()-1) {
+            System.out.println("YUP");
+            return true;
+        }
+
         return currentIndex == (this.previousStates.size() - 1) || this.previousStates.size() == 0;
     }
 
     public boolean isPresent() {
-        return currentIndex == this.previousStates.size();
+        return currentIndex ==(this.previousStates.size() - 1);
     }
 }
